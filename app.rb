0d8cd44dd6a99840d05e7a21e7624cca6bfbd211
Daniel Trostli <daniel.trostli@gmail.com>
2013-09-16 18:36:47 -0700

    dans changes'

--- app.rb
+++ app.rb
@@ -5,3 +5,31 @@ set :database, "sqlite3:///blog.db"
 
 class User < ActiveRecord::Base
 end
+
+# get '/' do 
+# 	@users = User.order("name")
+# 	erb :"users/index"
+# end
+
+# get "/about" do
+#   @title = "About Us"
+#   erb :"pages/about"
+# end
+
+# helpers do
+#   # If @title is assigned, add it to the page's title.
+#   def title
+#     if @title
+#       "#{@title} -- My Blog"
+#     else
+#       "My Blog"
+#     end
+#   end
+ 
+#   # Format the Ruby Time object returned from a post's created_at method
+#   # into a string that looks like this: 06 Jan 2012
+#   def pretty_date(time)
+#    time.strftime("%d %b %Y")
+#   end
+ 
+# end
