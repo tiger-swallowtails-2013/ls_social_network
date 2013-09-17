3b67557b1a4f25d0cc6418ebb264ea60609e6917
dbc-warrior <apprentice@devbootcamp.com>
2013-09-17 15:21:00 -0700

    add authentication

--- social_media_app/app.rb
+++ social_media_app/app.rb
@@ -1,18 +1,37 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require_relative 'models/user'
+require_relative 'models/post'
+
+enable :sessions
 
 set :database, "sqlite3:///social_media.db"
 
-class User < ActiveRecord::Base
-  has_many :posts
-end
 
-class Post < ActiveRecord::Base
-  belongs_to :user
-end
+
 
 get '/' do
-  @user = User.first
+  session[:user] ||= nil
   erb :sign_in
+end
+
+
+post '/sign_in_verify' do
+  @user = User.find_by_name(params[:username])
+  
+  unless @user.nil? && @user.password != params[:password]
+    session[:user] = params[:username]
+    redirect '/user_home'
+  end
+  redirect '/'
+end
+
+
+get '/user_home' do
+  unless session[:user].nil?
+    erb :user_home
+  else
+    "OOPs you are not logged in"
+  end
 end
 
