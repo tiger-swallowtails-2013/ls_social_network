85bf1a7177c5cb08df8a346106907c73eed7f5d4
dbcapprentice <dbcapprentice@gmail.com>
2013-09-18 16:04:04 -0700

    add reverse relationship

--- social_media_app/models/user.rb
+++ social_media_app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
 
+  has_many :relationships, foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :relationships, source: :follower
+
 end
 
